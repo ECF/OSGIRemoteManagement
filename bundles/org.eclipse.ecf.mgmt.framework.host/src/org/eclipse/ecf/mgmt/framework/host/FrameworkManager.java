59c06d2c70e60db6b87b471bfdbdba6cbef5c5b8
slewis <slewis@composent.com>
2015-02-19 07:48:21 -0800

    Simplifications

--- bundles/org.eclipse.ecf.mgmt.framework.host/src/org/eclipse/ecf/mgmt/framework/host/FrameworkManager.java
+++ bundles/org.eclipse.ecf.mgmt.framework.host/src/org/eclipse/ecf/mgmt/framework/host/FrameworkManager.java
@@ -73,8 +73,7 @@ public class FrameworkManager extends AbstractManager implements IFrameworkManag
 
 	@Override
 	public void setInitialBundleStartLevel(int initialBundleStartLevel) {
-		final FrameworkStartLevel fsl = getBundle0(0).adapt(FrameworkStartLevel.class);
-		fsl.setInitialBundleStartLevel(initialBundleStartLevel);
+		getBundle0(0).adapt(FrameworkStartLevel.class).setInitialBundleStartLevel(initialBundleStartLevel);
 	}
 
 }
