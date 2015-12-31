ed79ffd2d5e6d7ba3e53d9d1cd61469c0c165670
slewis <slewis@composent.com>
2015-12-31 14:55:01 -0800

    Initial checkin of org.eclipse.ecf.mgmt.consumer.util bundle

---
+++ bundles/org.eclipse.ecf.mgmt.consumer.util/src/org/eclipse/ecf/mgmt/consumer/util/RemoteServiceEvent.java
@@ -0,0 +1,31 @@
+/*******************************************************************************
+ * Copyright (c) 2015 Composent, Inc. All rights reserved. This
+ * program and the accompanying materials are made available under the terms of
+ * the Eclipse Public License v1.0 which accompanies this distribution, and is
+ * available at http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors: Scott Lewis - initial API and implementation
+ ******************************************************************************/
+package org.eclipse.ecf.mgmt.consumer.util;
+
+public class RemoteServiceEvent {
+
+	public static final int ADDED = 1;
+	public static final int REMOVED = 2;
+	
+	private final int eventType;
+	private final RemoteServiceHolder holder;
+	
+	public RemoteServiceEvent(int type, RemoteServiceHolder holder) {
+		this.eventType = type;
+		this.holder = holder;
+	}
+	
+	public int getType() {
+		return this.eventType;
+	}
+	
+	public RemoteServiceHolder getRemoteServiceHolder() {
+		return this.holder;
+	}
+}
